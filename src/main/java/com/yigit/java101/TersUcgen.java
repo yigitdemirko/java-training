c19023ec8bfb0f69fd071849e19bda67edb12eb3
Yigit Demirko <ygtdemirko@gmail.com>
2026-04-20 21:43:58 +0300

    feat: add inverted triangle star pattern

---
+++ src/main/java/com/yigit/java101/TersUcgen.java
@@ -0,0 +1,19 @@
+package com.yigit.java101;
+
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Basamak Sayısı: ");
+        int n = scanner.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
